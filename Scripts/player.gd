346fbfb1586200920752f2587bb865a114a12a2e
Alejandro <a.valdez9265@gmail.com>
2026-03-06 13:28:13 -0800

    developing player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,40 @@
 extends Node2D
 
-var direction: Vector2 = Vector2.ZERO
-var speed: int = 10
+const PLAYER_START_POSITION = Vector2(0, 418)
+const POSITION_INCREMENT = 16
 
-func _physics_process(delta: float) -> void:
-	position += direction * speed
+@onready var sprite_2d = $Sprite2D
+@onready var animation_player = $AnimationPlayer	
+
+var new_position: Vector2 = Vector2.ZERO
+@export var speed = 40
+
+func _process(delta):
+	if new_position == Vector2.ZERO:
+		return
+	position = lerp(position, new_position, speed * delta)
+	
+	if absf((position - new_position).length() ) < 0.001:
+		position = round(position)
+
+func _input(event):
+	
+	var position_candidate
+	
+	if Input.is_action_just_pressed("down"):
+		position_candidate = Vector2.DOWN * POSITION_INCREMENT + position
+		#sprite_2d.rotation_degrees = 180
+	elif Input.is_action_just_pressed("up"):
+		position_candidate = Vector2.UP * POSITION_INCREMENT + position
+		#sprite_2d.rotation_degrees = 0
+	elif Input.is_action_just_pressed("left"):
+		position_candidate = Vector2.LEFT * POSITION_INCREMENT + position
+		#sprite_2d.rotation_degrees = 270
+	elif Input.is_action_just_pressed("right"):
+		#sprite_2d.rotation_degrees = 90
+		position_candidate = Vector2.RIGHT * POSITION_INCREMENT + position
+
+	if !position_candidate:
+		return
+	new_position = position_candidate
 	
